622c236954e62654e3a6f4b804110031e710801e
Ilyas Mahjoubi <ilmahjou@c2r6p11.42firenze.it>
2025-06-30 19:39:12 +0200

    add .h

---
+++ cudb3d.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cudb3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilmahjou <ilmahjou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 17:23:13 by ilmahjou          #+#    #+#             */
+/*   Updated: 2025/06/30 19:35:28 by ilmahjou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+# include "./mlx/mlx.h"
+# include "./mlx/mlx_int.h"
+# include "./libft/libft.h"
+# include "./get_next_line/get_next_line.h"
+# include "./libprintf/ft_printf.h"
+# include <limits.h>
+// # include <>
+/* Window and game constants */
+# define SCREEN_WIDTH 1024
+# define SCREEN_HEIGHT 768
+# define TEX_WIDTH 64
+# define TEX_HEIGHT 64
+
+
+#endif
